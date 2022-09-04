fa2a029f45e0a4d14eb7f910fe981bc6cc32a8b5
kawsar42 <59926691+kawsar42@users.noreply.github.com>
2022-09-04 19:54:58 +0600

    Add files via upload

---
+++ Convert string into interger.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    int a=123;
+    string s=to_string(a);
+    s[1]='9';
+    cout << s << endl;
+    string st="234";
+    int b = stoi(st);
+    b++;
+    cout << b << endl;
+
+
+
+}
